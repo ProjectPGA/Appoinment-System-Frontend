c65712a888eab64af1a5dd8c1832db12a9d4e63d
pablo <villasana.pablo.jobs@gmail.com>
2019-08-27 13:54:31 +0200

    touch query and api ip

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
-const baseURL = 'http://localhost:3000';
+const baseURL = 'http://80.102.112.116:3000';
 axios.defaults.baseURL = baseURL;
 Vue.use(VueAxios, axios);
