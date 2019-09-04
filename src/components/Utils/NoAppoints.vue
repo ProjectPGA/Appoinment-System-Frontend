208ae641906ff901c2a392fc520afb23d4c1c529
pablo <villasana.pablo.jobs@gmail.com>
2019-09-04 09:15:22 +0200

    aux classes added

---
+++ src/components/Utils/NoAppoints.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <div class="column is-12" v-if="citasView.length === 0">
+            <section class="hero is-danger">
+                <div class="hero-body">
+                    <div class="container">
+                        <h1 class="title">
+                            Ninguna cita Asignada
+                        </h1>
+                        <h2 class="subtitle">
+                            <router-link to="/inicio"
+                                >Puedes asignarte una cita pulsando
+                                aquí</router-link
+                            >
+                        </h2>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import { State } from 'vuex-class';
+import { ViewAppointment } from '@/models/appointment/Appointment';
+
+@Component({
+    name: 'NoAppoints',
+})
+export default class NoAppoints extends Vue {
+    @Prop() private citasView: ViewAppointment[];
+}
+</script>
+
+<style lang="scss" scoped></style>
