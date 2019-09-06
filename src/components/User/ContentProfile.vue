601f9bec416a0d155ef1e6a11c61ae4ef4ace67a
pablo <villasana.pablo.jobs@gmail.com>
2019-09-06 16:18:06 +0200

    user config layout y location.en.ts

--- src/components/User/ContentProfile.vue
+++ src/components/User/ContentProfile.vue
@@ -1,17 +1,29 @@
 <template>
-    <div>
-        Content Profile
+    <div class="column is-9 custom-border">
+        <user-config-info></user-config-info>
+        <user-config-security></user-config-security>
     </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
- 
+import UserConfigInfo from '@/components/User/UserConfigInfo.vue';
+import UserConfigSecurity from '@/components/User/UserConfigSecurity.vue';
+
 @Component({
     name: 'ContentProfile',
-    components: {},
+    components: {
+        UserConfigInfo,
+        UserConfigSecurity,
+    },
 })
 export default class ContentProfile extends Vue {}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.custom-border {
+    border-left: thin;
+    border-left-style: solid;
+    border-left-color: #4a4a4aab;
+}
+</style>
---
+++ src/components/User/UserConfigMenu.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="column is-3">
+        <b-menu>
+            <b-menu-list class="custom-menu-title" icon-pack="fas" icon="cog" :label="$i18n.t('userConfig.configText')">
+                <b-menu-item icon="user" active expanded>
+                    <template slot="label" slot-scope="props">
+                        {{$t('userConfig.userText')}}
+                        <b-icon
+                            class="is-pulled-right"
+                            pack="fas"
+                            :icon="props.expanded ? 'menu-down' : 'menu-up'"
+                        >
+                        </b-icon>
+                    </template>
+                    <b-menu-item
+                        @click="setSelectedSettings('info')"
+                        icon="info"
+                        :label="$i18n.t('userConfig.user.infoText')"
+                    ></b-menu-item>
+                    <b-menu-item
+                        @click="setSelectedSettings('security')"
+                        icon-pack="fas"
+                        icon="lock"
+                        :label="$i18n.t('userConfig.user.securityText')"
+                    ></b-menu-item>
+                </b-menu-item>
+                <b-menu-item icon="user-shield" label="Administrador">
+                    <b-menu-item :label="$i18n.t('userConfig.adminText')"></b-menu-item>
+                </b-menu-item>
+            </b-menu-list>
+        </b-menu>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import { Mutation } from 'vuex-class';
+
+@Component({
+    name: 'UserConfigMenu',
+    components: {},
+})
+export default class UserConfigMenu extends Vue {
+    @Mutation('utils/setSelectedSettingsMenu') private setSelectedSettings: (
+        menu: string
+    ) => void;
+}
+</script>
+
+<style lang="scss" scoped>
+.menu-label{
+    font-size: 1rem !important;
+}
+</style>
