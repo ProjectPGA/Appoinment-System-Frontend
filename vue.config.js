5a9e9923197d7a1d31884f0ef9df56143cee889b
Pablo <villasana.pablo.jobs@gmail.com>
2021-01-10 17:49:32 +0100

    Main App > Config: API address changed for the real one

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ module.exports = {
         port: 18000,
         proxy: {
             '/api': {
-                target: 'http://localhost:3000/',
+                target: 'http://45.9.190.165:3000/',
                 ws: true,
                 changeOrigin: true,
                 cookieDomainRewrite: true,
