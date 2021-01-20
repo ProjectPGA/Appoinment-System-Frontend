3219aa1792fb7994127c9cc485aec7f8ade89abc
andresws <andreshernandez12.1995@gmail.com>
2021-01-20 19:10:43 +0100

    Use localhost on const from webservice

--- src/webservices/consts.ts
+++ src/webservices/consts.ts
@@ -1,6 +1,5 @@
 // TODO. Just have this in a core module
-// export const API_PREFIX = 'http://45.9.190.165:3000/api/';
-export const API_PREFIX = '/api';
+export const API_PREFIX = 'http://localhost:3000/api';
 
 export const apiPrefix = (url: string): any => API_PREFIX + url;
 
