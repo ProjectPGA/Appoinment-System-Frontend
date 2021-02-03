cbe6bd8387f448fa3c1ae6f1e341866aea12947c
JuanJosePG <jjgp.code@gmail.com>
2021-02-03 16:59:32 +0100

    Main > Views: BEM Methodology implemented

--- src/views/MaintenancePage.vue
+++ src/views/MaintenancePage.vue
@@ -1,15 +1,19 @@
 <template>
-    <div class="maintenance-page-container">
-        <div class="maintenance-image-container">
+    <div class="maintenance-page">
+        <div class="maintenance-page_image-container">
             <img
-                class="maintenance-image"
+                class="maintenance-page_image"
                 src="../assets/img/maintenance-image-1080x720.svg"
-                alt=""
+                alt="Maintenance Page Image"
             />
         </div>
-        <div class="maintenance-text-container">
-            <h2 class="maintenance-title">{{ $t('maintenance.title') }}</h2>
-            <p class="maintenance-subtitle">{{ $t('maintenance.subtitle') }}</p>
+        <div class="maintenance-page_text-container">
+            <h1 class="maintenance-page_title">
+                {{ $t('maintenance.title') }}
+            </h1>
+            <h2 class="maintenance-page_subtitle">
+                {{ $t('maintenance.subtitle') }}
+            </h2>
         </div>
     </div>
 </template>
@@ -24,47 +28,47 @@ export default class MaintenancePage extends Vue {}
 </script>
 
 <style lang="scss" scoped>
-.maintenance-page-container {
+.maintenance-page {
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-}
 
-.maintenance-image-container {
-    margin: 5rem 0 1.5rem 0;
-}
+    &_image-container {
+        margin: 5rem 0 1.5rem 0;
 
-.maintenance-image {
-    width: 40rem;
+        &_image {
+            width: 40rem;
 
-    @include mobile {
-        width: 15rem;
+            @include mobile {
+                width: 15rem;
+            }
+        }
     }
-}
 
-.maintenance-text-container {
-    text-align: center;
-    color: var(--blue-sapphire);
-}
+    &_text-container {
+        text-align: center;
+        color: var(--blue-sapphire);
 
-.maintenance-title {
-    font-size: 3.5rem;
-    font-weight: bold;
-    margin: 0 1.25rem;
-    text-shadow: 1px 1px var(--gunmetal);
+        &_title {
+            font-size: 3.5rem;
+            font-weight: bold;
+            margin: 0 1.25rem;
+            text-shadow: 1px 1px var(--gunmetal);
 
-    @include mobile {
-        font-size: 2rem;
-    }
-}
+            @include mobile {
+                font-size: 2rem;
+            }
+        }
 
-.maintenance-subtitle {
-    font-size: 2.5rem;
-    margin: 1rem 1.5rem;
+        &_subtitle {
+            font-size: 2.5rem;
+            margin: 1rem 1.5rem;
 
-    @include mobile {
-        font-size: 1.5rem;
+            @include mobile {
+                font-size: 1.5rem;
+            }
+        }
     }
 }
 </style>
