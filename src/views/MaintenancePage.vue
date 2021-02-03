fcbc056b036f6c6407a49c90449e83dbb46834a2
JuanJosePG <jjgp.code@gmail.com>
2021-02-03 17:16:21 +0100

    Main > Vue Maint. Page: ButtonTranslation added

--- src/views/MaintenancePage.vue
+++ src/views/MaintenancePage.vue
@@ -2,27 +2,32 @@
     <div class="maintenance-page">
         <div class="maintenance-page_image-container">
             <img
-                class="maintenance-page_image"
+                class="maintenance-page_image-container_image"
                 src="../assets/img/maintenance-image-1080x720.svg"
                 alt="Maintenance Page Image"
             />
         </div>
         <div class="maintenance-page_text-container">
-            <h1 class="maintenance-page_title">
+            <h1 class="maintenance-page_text-container_title">
                 {{ $t('maintenance.title') }}
             </h1>
-            <h2 class="maintenance-page_subtitle">
+            <h2 class="maintenance-page_text-container_subtitle">
                 {{ $t('maintenance.subtitle') }}
             </h2>
         </div>
+        <button-translation></button-translation>
     </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import ButtonTranslation from '@/components/Login/ButtonTranslation.vue';
 
 @Component({
     name: 'MaintenancePage',
+    components: {
+        ButtonTranslation,
+    },
 })
 export default class MaintenancePage extends Vue {}
 </script>
@@ -38,8 +43,7 @@ export default class MaintenancePage extends Vue {}
         margin: 5rem 0 1.5rem 0;
 
         &_image {
-            width: 40rem;
-
+            width: 45rem;
             @include mobile {
                 width: 15rem;
             }
