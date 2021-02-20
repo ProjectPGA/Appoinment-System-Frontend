348058bff1dd8c4bbee6c8bf7d66550ec332d26f
Andresws12 <47737142+Andresws12@users.noreply.github.com>
2021-02-20 12:06:28 +0100

    Refactor and optimize login (#26)
    * Commit test
    
    * Translation button: Optimize and add variables to button translations
    
    * Login > email input: Add a new component with validations to email inputs
    
    * Login > Password input: Add a new component to valid the password and add disabled button
    
    * Login > Css: Opmtimize and refactor style classes from login
    
    * Login: Remove unneccesary login and variables.
    
    * Login:  Fix camel case, translations and add data-cy from new inputs

--- src/components/Login/MainLoginForm.vue
+++ src/components/Login/MainLoginForm.vue
@@ -1,63 +1,51 @@
 <template>
-    <form>
-        <button-translation></button-translation>
+    <form class="main-login-form">
+        <button-translation />
         <div class="columns is-centered">
-            <div class="column login-logo">
-                <logo-app data-cy="login-logo"></logo-app>
+            <div class="column main-login-form_logo">
+                <logo-app data-cy="login-logo" />
             </div>
         </div>
         <div class="columns is-centered is-mobile">
             <div class="column is-6-desktop is-10-mobile is-8-tablet container">
-                <p class="title">{{ $t('titles.login') }}</p>
-                <b-field
-                    :label="$t('components.login.email')"
-                    :type="bfieldType"
-                >
-                    <b-input
-                        v-model="email"
-                        :placeholder="$t('components.login.email')"
-                        size="is-medium"
-                        @keyup.native.enter="checkLogin()"
-                        data-cy="email"
-                    >
-                    </b-input>
-                </b-field>
+                <h1 class="main-login-form_title title">
+                    {{ $t('titles.login') }}
+                </h1>
 
-                <b-field
-                    :label="$t('components.login.password')"
-                    :type="bfieldType"
-                >
-                    <b-input
-                        v-model="password"
-                        :placeholder="$t('components.login.password')"
-                        type="password"
-                        size="is-medium"
-                        class="custom-margin"
-                        @keyup.native.enter="checkLogin()"
-                        data-cy="password"
-                        password-reveal
-                    >
-                    </b-input>
-                </b-field>
+                <email-input
+                    @input="onEmailInput"
+                    @checkEmail="onCheckEmail"
+                    view="login"
+                />
+
+                <password-input
+                    @input="onPasswordInput"
+                    @checkPassword="onCheckPassword"
+                    view="login"
+                />
+
                 <div class="columns is-vcentered">
                     <div class="column is-3 is-2-fullhd">
                         <b-button
                             @click="checkLogin()"
-                            :loading="isLoading"
                             outlined
                             type="is-danger"
                             size="is-medium"
                             data-cy="submit"
+                            :disabled="isInvalidForm"
                         >
                             {{ $t('components.login.button') }}
                         </b-button>
                     </div>
                     <div class="column">
-                        <p class="custom-size" data-cy="invitation">
+                        <p
+                            class="main-login-form_invitation"
+                            data-cy="invitation"
+                        >
                             {{ $t('components.login.noAccount') }}
                             <span
                                 @click="enableRegisterProgress"
-                                class="invitation-link"
+                                class="main-login-form_invitation-link"
                             >
                                 {{ $t('components.login.accessToInvitation') }}
                             </span>
@@ -70,144 +58,95 @@
 </template>
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import { SnackbarProgrammatic as Snackbar } from 'buefy';
 
 import authStore from '@/store/auth-store/AuthStore';
 
 import LogoApp from '@/components/Navigation/LogoApp.vue';
-import ButtonTranslation from '@/components/Login/ButtonTranslation.vue';
+import EmailInput from '@/components/Login/EmailInput.vue';
+import PasswordInput from '@/components/Login/PasswordInput.vue';
+import ButtonTranslation from '@/components/common/ButtonTranslation.vue';
+
 import { LoginRequest } from '@/webservices/models/auth/LoginRequest';
 
 @Component({
     name: 'MainLoginForm',
     components: {
         LogoApp,
+        EmailInput,
+        PasswordInput,
         ButtonTranslation,
     },
 })
 export default class MainLoginForm extends Vue {
-    private email: string = '';
-    private password: string = '';
-    private isLoading: boolean = false;
-    private bfieldType: string = '';
-
     private authStore = authStore.context(this.$store);
 
+    private email: string = '';
+    private isEmailVaild: boolean = true;
+
+    private password: string = '';
+    private isPasswordVaild: boolean = true;
+
+    private get isInValidform(): boolean {
+        return this.email === '' && this.password === ''
+            ? true
+            : !this.isEmailVaild || !this.isPasswordVaild
+            ? true
+            : false;
+    }
+
+    private onEmailInput(email: string): void {
+        this.email = email;
+    }
+
+    private onCheckEmail(isEmailVaild: boolean): void {
+        this.isEmailVaild = isEmailVaild;
+    }
+
+    private onPasswordInput(password: string): void {
+        this.password = password;
+    }
+
+    private onCheckPassword(isPasswordVaild: boolean): void {
+        this.isPasswordVaild = isPasswordVaild;
+    }
+
     private async checkLogin() {
-        const validator = this.loginValidator();
-        if (validator) {
-            this.isLoading = true;
-            try {
-                const loginData: LoginRequest = {
-                    email: this.email,
-                    password: this.password,
-                };
+        const loginData: LoginRequest = {
+            email: this.email,
+            password: this.password,
+        };
 
-                this.authStore.actions.login({ loginData });
-
-                // if (!response.data.cod || response.status === 404) {
-                //     this.bfieldType = 'is-danger';
-                //     this.isLoading = false;
-                //     Snackbar.open({
-                //         message: response.data.mensaje,
-                //         type: 'is-danger',
-                //         position: 'is-bottom-left',
-                //         duration: 3000,
-                //         actionText: 'Volver a intentar',
-                //         onAction: () => {
-                //             this.clearInputs();
-                //         },
-                //     });
-                // } else {
-                //     this.isLoading = false;
-                //     this.saveUser(response.data.user);
-                //     router.push('/inicio');
-                // }
-            } catch (error) {
-                this.isLoading = false;
-                Snackbar.open({
-                    message: 'Error en la conexión',
-                    type: 'is-danger',
-                    position: 'is-bottom-left',
-                    duration: 3000,
-                    actionText: 'Volver a intentar',
-                    onAction: () => {
-                        this.clearInputs();
-                    },
-                });
-                // TODO: Show Error
-                // console.log(error);
-            }
-        }
+        this.authStore.actions.login({ loginData });
     }
 
     private enableRegisterProgress(): void {
         this.authStore.actions.enableRegisterProcess();
         this.$router.push('/invitation');
     }
-
-    private clearInputs(): void {
-        this.email = '';
-        this.password = '';
-        this.bfieldType = '';
-    }
-    private loginValidator(): boolean {
-        if (this.email.length === 0 || this.password.length === 0) {
-            this.bfieldType = 'is-danger';
-            Snackbar.open({
-                message: 'Por favor introduzca la informacion requerida',
-                type: 'is-danger',
-                position: 'is-bottom-left',
-                duration: 3000,
-                actionText: 'Volver a intentar',
-                onAction: () => {
-                    this.clearInputs();
-                },
-            });
-            return false;
-        } else {
-            return true;
-        }
-    }
 }
 </script>
 
 <style lang="scss" scoped>
-.margin-rigth {
-    @include desktop {
-        margin-right: 4%;
+.main-login-form {
+    &_invitation-link {
+        cursor: pointer;
+        color: $main-color !important;
+        &:hover {
+            text-decoration-line: underline;
+        }
     }
-}
-.invitation-link {
-    color: $main-color !important;
-}
-.custom-size {
-    font-size: 1.25em;
-}
-.title {
-    font-family: 'CabbageTown';
-    @include mobile {
+
+    &_title {
         font-size: calc(0.75em + 0.5vw);
+        line-height: 3em !important;
     }
-    font-size: calc(0.5em + 0.5vw);
-    line-height: 3em !important;
-    color: $main-color-dark;
-}
-.custom-login-input {
-    width: 50%;
-    @include mobile {
-        width: 85%;
+
+    &_invitation {
+        font-size: 1.25em;
     }
-}
-.centered-content {
-    display: flex !important;
-    justify-content: center !important;
-}
-.custom-margin {
-    margin-bottom: 6%;
-}
-.login-logo {
-    margin-top: 50px;
-    margin-bottom: 50px;
+    &_logo {
+        margin-top: 50px;
+        margin-bottom: 50px;
+    }
 }
 </style>
---
+++ src/components/Login/PasswordInput.vue
@@ -0,0 +1,77 @@
+<template>
+    <b-field
+        :label="$t('components.loginInputs.password')"
+        :message="errorMessage"
+        :type="{
+            'is-danger': !isVaild,
+        }"
+    >
+        <b-input
+            v-model="password"
+            :placeholder="$t('components.loginInputs.password')"
+            size="is-medium"
+            :data-cy="`${view}-input-password`"
+            type="password"
+            password-reveal
+            required
+            @blur="checkPassword"
+            @input="onInput"
+            class="password-input"
+        />
+    </b-field>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component({
+    name: 'PasswordInput',
+})
+export default class PasswordInput extends Vue {
+    @Prop(String) private view: string;
+
+    private password: string = '';
+    private isVaild: boolean = true;
+    private errorMessage: string = '';
+
+    private onInput(): void {
+        this.$emit('input', this.password);
+        this.checkPassword();
+    }
+
+    private checkPassword(): void {
+        this.password === ''
+            ? this.inputEmpty()
+            : this.password.length >= 8
+            ? this.inputValid()
+            : this.inputPasswordInvalid();
+
+        this.$emit('checkPassword', this.isVaild);
+    }
+
+    private inputEmpty(): void {
+        this.errorMessage = `${this.$t('components.loginInputs.inputEmpty')}`;
+        this.isVaild = false;
+    }
+
+    private inputPasswordInvalid(): void {
+        this.errorMessage = `${this.$t(
+            'components.loginInputs.passwordInvalid'
+        )}`;
+        this.isVaild = false;
+    }
+
+    private inputValid(): void {
+        this.errorMessage = '';
+        this.isVaild = true;
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.password-input {
+    /deep/.icon {
+        color: $main-color-medium-light !important;
+    }
+}
+</style>
