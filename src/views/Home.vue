8657c34c92cebe9e6302adef7cfbb877fbcaf4ed
Andresws12 <andreshernandez12.1995@gmail.com>
2021-03-26 12:53:46 +0100

    Remove wath

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -88,7 +88,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue, Watch } from 'vue-property-decorator';
+import { Component, Vue } from 'vue-property-decorator';
 
 import authStore from '@/store/auth-store/AuthStore';
 
