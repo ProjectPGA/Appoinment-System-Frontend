63893587acab595616867b0518bd86f5bb3cd690
gonzalo-plaza <plazaruedag@gmail.com>
2021-05-30 19:57:41 +0200

    [FIX] Duplicate selector: Use the selector in the view file, and run format.

--- tests/e2e/specs/utilities/02Invitational.selectors.ts
+++ tests/e2e/specs/utilities/02Invitational.selectors.ts
@@ -1,5 +1,4 @@
 // Invitational
 
-export const invitationalLink = '[data-cy="login-invitation-link"]';
 export const invitationalCodeInput = '[data-cy="invitational-code-input"]';
 export const invitationalCodeButton = '[data-cy="invitational-code-button"]';
