d63e22e8c36a240a6d1fb0535c7aff6ce8134629
gonzalo-plaza <plazaruedag@gmail.com>
2021-06-03 16:13:26 +0200

    [ADD] Test e2e: Add test e2e to invitational view.

--- tests/e2e/specs/02Invitation.spec.ts
+++ tests/e2e/specs/02Invitation.spec.ts
@@ -2,13 +2,80 @@ import { getElem } from './utilities/utils';
 
 import {
     logoApp,
+    toastMainSelector,
     translationButtonSelector,
 } from './utilities/common.selectors';
 
 import { toastErrorClass } from './utilities/common.classes';
+
+import { loginInvitationLink } from './utilities/01Login.selectors';
 
 import {
     invitationalCodeTitle,
     invitationalCodeInput,
     invitationalCodeButton,
 } from './utilities/02Invitational.selectors';
+
+describe('02 Invitational', () => {
+    beforeEach(() => {
+        cy.visit('/');
+        getElem(loginInvitationLink).click();
+    });
+
+    it('02 Invitational', () => {
+        cy.log('02.01 Check all elements are visible');
+        getElem(logoApp).should('be.visible');
+        getElem(translationButtonSelector).should('be.visible');
+        getElem(invitationalCodeTitle).should('be.visible');
+        getElem(invitationalCodeInput).should('be.visible');
+        getElem(invitationalCodeButton)
+            .should('be.visible')
+            .should('be.disabled');
+
+        cy.log('02.02 Check logoApp is function');
+        getElem(logoApp).click();
+        cy.url().should('include', '/');
+        getElem(loginInvitationLink).click();
+
+        cy.log(
+            '02.03 Check when the invitation code has less of 11 characters'
+        );
+        getElem(invitationalCodeInput)
+            .should('have.value', '')
+            .type('11111111111');
+        getElem(invitationalCodeButton)
+            .should('be.visible')
+            .should('be.disabled');
+
+        cy.log(
+            '02.04 Check when the invitation code has 12 characters but doesn´t exist'
+        );
+        getElem(invitationalCodeInput).type('1');
+        getElem(invitationalCodeButton)
+            .should('be.visible')
+            .should('be.enabled')
+            .click();
+        getElem(toastMainSelector)
+            .should('be.visible')
+            .should('have.class', toastErrorClass);
+        cy.wait(4020);
+        getElem(toastMainSelector).should('not.be.visible');
+
+        cy.log(
+            '02.05 Check when the invitation code has more of 11 characters'
+        );
+        getElem(invitationalCodeInput).type('1');
+        getElem(invitationalCodeButton)
+            .should('be.visible')
+            .should('be.disabled');
+
+        cy.log('02.06 Check when the invitation code exist');
+        getElem(invitationalCodeInput).clear().type('MSINVITATION');
+        getElem(invitationalCodeButton)
+            .should('be.visible')
+            .should('be.enabled')
+            .click();
+        cy.url().should('include', '/register');
+        getElem(logoApp).click();
+    });
+});
