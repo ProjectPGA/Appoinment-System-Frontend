f45b6e7fce777e24fca23b1ecd12f8873f854d46
gonzalo-plaza <plazaruedag@gmail.com>
2021-06-07 14:27:24 +0200

    [FIX] Selectors and actions: Group selectors and separe should of actions in the test.

--- tests/e2e/specs/02Invitation.spec.ts
+++ tests/e2e/specs/02Invitation.spec.ts
@@ -3,18 +3,18 @@ import { getElem } from './utilities/utils';
 import {
     logoApp,
     toastMainSelector,
+    toastCloseButtonSelector,
     translationButtonSelector,
 } from './utilities/common.selectors';
-
-import { toastErrorClass } from './utilities/common.classes';
-
-import { loginInvitationLink } from './utilities/01Login.selectors';
 
 import {
     invitationalCodeTitle,
     invitationalCodeInput,
     invitationalCodeButton,
 } from './utilities/02Invitational.selectors';
+
+import { loginInvitationLink } from './utilities/01Login.selectors';
+import { toastErrorClass } from './utilities/common.classes';
 
 describe('02 Invitational', () => {
     beforeEach(() => {
@@ -34,15 +34,18 @@ describe('02 Invitational', () => {
 
         cy.log('02.02 Check if logoApp is working');
         getElem(logoApp).click();
+
         cy.url().should('include', '/');
+
         getElem(loginInvitationLink).click();
 
         cy.log(
             '02.03 Check when the invitation code has less than 11 characters'
         );
-        getElem(invitationalCodeInput)
-            .should('have.value', '')
-            .type('11111111111');
+        getElem(invitationalCodeInput).should('have.value', '');
+
+        getElem(invitationalCodeInput).type('11111111111');
+
         getElem(invitationalCodeButton)
             .should('be.visible')
             .should('be.disabled');
@@ -51,32 +54,39 @@ describe('02 Invitational', () => {
             '02.04 Check when the invitation code has 12 characters but doesn´t exist'
         );
         getElem(invitationalCodeInput).clear().type('111111111111');
+
         cy.intercept('/api/auth/invitation').as('invitation');
         getElem(invitationalCodeButton)
             .should('be.visible')
-            .should('be.enabled')
-            .click();
+            .should('be.enabled');
+
+        getElem(invitationalCodeButton).click();
+
         cy.wait('@invitation').its('response.statusCode').should('eq', 400);
         getElem(toastMainSelector)
             .should('be.visible')
             .should('have.class', toastErrorClass);
-        cy.wait(4020);
-        getElem(toastMainSelector).should('not.be.visible');
+
+        getElem(toastCloseButtonSelector).click();
 
         cy.log(
             '02.05 Check when the invitation code has more than 11 characters'
         );
         getElem(invitationalCodeInput).clear().type('1111111111111');
+
         getElem(invitationalCodeButton)
             .should('be.visible')
             .should('be.disabled');
 
         cy.log('02.06 Check when the invitation code exist');
         getElem(invitationalCodeInput).clear().type('MSINVITATION');
+
         getElem(invitationalCodeButton)
             .should('be.visible')
-            .should('be.enabled')
-            .click();
+            .should('be.enabled');
+
+        getElem(invitationalCodeButton).click();
+
         cy.wait('@invitation').its('response.statusCode').should('eq', 200);
         cy.url().should('include', '/register');
     });
